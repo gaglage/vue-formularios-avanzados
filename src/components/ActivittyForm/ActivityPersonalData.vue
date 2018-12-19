374fec8c62be9c16d0d34cda6d9d68cafebc71f3
gaglage <gabialvarez@gmail.com>
2018-12-19 17:30:52 +0100

    component personal data and validate

---
+++ src/components/ActivittyForm/ActivityPersonalData.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-card
+    class="mt-3"
+    title="Datos personales"
+  >
+    <b-row>
+      <b-col>
+        <b-form-group label="Nombre">
+          <b-form-input
+            :state="$parent.validateState('name')"
+            autocomplete="off"
+            data-vv-as="nombre"
+            name="name"
+            placeholder="Introduce tu nombre"
+            v-model="personal_data.name"
+            v-validate="'required|min:2'"
+          ></b-form-input>
+          <b-form-invalid-feedback>{{ errors.first('name') }}</b-form-invalid-feedback>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group label="Apellidos">
+          <b-form-input
+            :state="$parent.validateState('surname')"
+            autocomplete="off"
+            data-vv-as="apellidos"
+            name="surname"
+            placeholder="Introduce tus apellidos"
+            v-model="personal_data.surname"
+            v-validate="'required|min:4'"
+          ></b-form-input>
+          <b-form-invalid-feedback>{{ errors.first('surname') }}</b-form-invalid-feedback>
+        </b-form-group>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+ inject: ['$validator'],
+  props: {
+    personal_data: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="mt-5">Activities</div>
+  <b-form
+    @submit.prevent="submitActivity"
+    class="mt-5"
+  >
+  <activity-personal-data :personal_data="activity.personal_data"></activity-personal-data>
+    <b-row>
+      <b-col>
+        <b-btn
+          type="submit"
+          variant="primary"
+        >Submit</b-btn>
+      </b-col>
+    </b-row>
+  </b-form>
 </template>
 
 <script>
+import ActivityPersonalData from "../components/ActivittyForm/ActivityPersonalData";
+import VaidationMixin from '../mixins/validation'
 export default {
+  components: {
+    ActivityPersonalData
+  },
+  mixins:[VaidationMixin],
   data() {
     return {
       reset: true,
@@ -25,6 +44,11 @@ export default {
         }
       }
     };
+  },
+  methods: {
+    async submitActivity() {
+      const validate = await this.$validator.validateAll();
+    }
   }
 };
 </script>
